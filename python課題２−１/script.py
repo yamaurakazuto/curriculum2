ff209dc43761f47410fe34fad2e20211d4319de1
yamaurakazuto <143419323+yamaurakazuto@users.noreply.github.com>
2023-10-06 20:55:05 +0900

    課題提出
    python課題2-1,2-2の作成

---
+++ python課題２−１/script.py
@@ -0,0 +1,26 @@
+all_place = ["札幌","東京","横浜","大阪","名古屋","福岡"]
+wait_place = ["札幌","大阪"]
+get_place = ["横浜"]
+
+for place in all_place:
+    #ここから記述（ヒント：in演算子を用いて、条件分岐）
+    if place == "札幌":
+        print(place + "のチケットは結果待ち")
+    #ここから記述（ヒント：in演算子を用いて、条件分岐
+    elif place == "東京":
+        print(place + "のチケットは落選しました")
+    elif place == "横浜":
+        print( place+ "のチケットが当選しました")
+    elif all_place == "大阪":
+        print(place + "のチケットは結果待ち")
+    elif all_place == "名古屋":
+        print(place + "のチケットは落選しました")
+    elif all_place == "福岡":
+        print(place + "のチケットは落選しました")
+        #科第２−２
+    elif (place in get_place for place in wait_place):
+        print("横浜と札幌と大阪のチケットが当選しました")
+
+
+    else:
+        print(place + "のチケットは落選しました")
